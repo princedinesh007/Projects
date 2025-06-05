c8f02ed9aefbb67b76064712ba5a38fde7bccc4d
Dineshkumar Arunachalam <dineshkumar@ssdiblr.sharp.co.in>
2025-06-05 12:05:47 +0530

    hamburger menu

--- Angular_Pizza/src/app/header/header.component.ts
+++ Angular_Pizza/src/app/header/header.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink,RouterLinkActive,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+   menuOpened = false;  // Variable to control the menu state
+
+  // Toggle the menu open and close
+  toggleMenu() {
+    this.menuOpened = !this.menuOpened;
+  }
 
 }
